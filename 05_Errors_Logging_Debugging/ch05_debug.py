1feef65cfc947b3cc27da8bb8da952bc66018d25
Catherine Nelson <catherine.nelson1@gmail.com>
2024-10-30 12:35:22 -0700

    remove breakpoint and update ch2 figure

--- 05_Errors_Logging_Debugging/ch05_debug.py
+++ 05_Errors_Logging_Debugging/ch05_debug.py
@@ -2,7 +2,6 @@ def weighted_mean(num_list, weights):
     running_total = 0
     for i in range(len(num_list)):
         running_total += num_list[i] * weights[0]
-        breakpoint()
     return running_total / len(num_list)
 
 
